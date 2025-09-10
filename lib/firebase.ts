bc3770f359d36a7441b9437b2b3d82e36c0a4fda
jordanwits <jordanwitbeck17@gmail.com>
2025-09-10 10:05:24 -0700

    Feat: Move Firebase keys to .env.local

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,13 +6,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCS5pq5qiv4dde2H_rRaol4AEpclBsxD90",
-  authDomain: "wpwater-332ac.firebaseapp.com",
-  projectId: "wpwater-332ac",
-  storageBucket: "wpwater-332ac.appspot.com",
-  messagingSenderId: "864910900635",
-  appId: "1:864910900635:web:4530cabbca3c1e705e353e",
-  measurementId: "G-T3732N3MHB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
